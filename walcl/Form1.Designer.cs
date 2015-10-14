b1a7238d52b51c03fb401542261304e65f3004f3
Urika <urika.wu@foxmail.com>
2015-10-15 02:05:27 +0800

    Add source code

---
+++ walcl/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace walcl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btExec = new System.Windows.Forms.Button();
+            this.tbInfo = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btExec
+            // 
+            this.btExec.Location = new System.Drawing.Point(561, 338);
+            this.btExec.Name = "btExec";
+            this.btExec.Size = new System.Drawing.Size(75, 23);
+            this.btExec.TabIndex = 0;
+            this.btExec.Text = "执行";
+            this.btExec.UseVisualStyleBackColor = true;
+            this.btExec.Click += new System.EventHandler(this.btExec_Click);
+            // 
+            // tbInfo
+            // 
+            this.tbInfo.Location = new System.Drawing.Point(12, 30);
+            this.tbInfo.Multiline = true;
+            this.tbInfo.Name = "tbInfo";
+            this.tbInfo.ReadOnly = true;
+            this.tbInfo.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tbInfo.ShortcutsEnabled = false;
+            this.tbInfo.Size = new System.Drawing.Size(651, 284);
+            this.tbInfo.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(675, 380);
+            this.Controls.Add(this.tbInfo);
+            this.Controls.Add(this.btExec);
+            this.Name = "Form1";
+            this.Text = "Office";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btExec;
+        private System.Windows.Forms.TextBox tbInfo;
+    }
+}
+
---
+++ walcl/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using MSExcel = Microsoft.Office.Interop.Excel;
+using MSWord = Microsoft.Office.Interop.Word;
+using System.IO;
+using System.Reflection;
+
+using System.Runtime.InteropServices;
+
+namespace walcl
+{
+    public partial class Form1 : Form
+    {
+        int intRows = 0;
+        int intCols = 0;
+        String[,] strData;
+        const String DOCPATH = @"D:\O\";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btExec_Click(object sender, EventArgs e)
+        {
+            if (LoadFromExcel())
+            {
+                WriteDocument();
+            }
+            strData = null;
+        }
+
+        public void WriteDocument()
+        {
+            MSWord.ApplicationClass wordApp = new MSWord.ApplicationClass();
+            MSWord.Document wordDoc = null;
+            object missing = System.Reflection.Missing.Value;
+            object readOnly = true;
+            object isVisible = true;
+            object TempletPath = DOCPATH + @"Templet.dotx";
+            try
+            {
+                //Generate all documents, start from the 2nd line
+                for (int i = 2; i <= intRows; i++)
+                {
+                    Log("Create document for " + strData[i - 1, 0] + "...");
+                    wordDoc = wordApp.Documents.Open(ref TempletPath, ref missing, ref readOnly,
+                            ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref isVisible, ref missing, ref missing, ref missing, ref missing);
+                    for (int j = 1; j <= intCols; j++)
+                    {
+                        object bkObj = strData[0, j - 1];
+                        if (wordApp.ActiveDocument.Bookmarks.Exists(strData[0, j - 1]))
+                        {
+                            wordApp.ActiveDocument.Bookmarks.get_Item(ref bkObj).Select();
+                            wordApp.Selection.TypeText(strData[i - 1, j - 1]);
+                        }
+                    }
+                    object sDocPath = DOCPATH + strData[i - 1, 0] + ".docx";
+
+                    object format = MSWord.WdSaveFormat.wdFormatDocumentDefault;
+                    wordDoc.SaveAs(ref sDocPath, ref format, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing);
+                    wordDoc.Close(ref missing, ref missing, ref missing);
+                }
+                wordApp.Quit(ref missing, ref missing, ref missing);
+                Log("Create OK");
+            }
+            catch (Exception e)
+            {
+                Log("Excel App error:" + e.ToString());
+                MessageBox.Show("WordApp App error:" + e.ToString());
+            }
+            finally
+            {
+                if (wordApp != null)
+                {
+                    Kill(wordApp);
+                    Log("Kill WordApp" + Environment.NewLine);
+                }
+            }
+
+        }
+
+        [DllImport("user32.dll")]
+        public static extern System.IntPtr FindWindowEx(System.IntPtr parent, System.IntPtr childe, string strclass, string strname);
+        public void Kill(MSWord.Application word)
+        {
+
+            IntPtr p = FindWindowEx(System.IntPtr.Zero, System.IntPtr.Zero, null, word.Caption);
+            int k = 0;
+
+            GetWindowThreadProcessId(p, out k);   //得到本进程唯一标志k  
+            if (k != 0)
+            {
+                System.Diagnostics.Process fp = System.Diagnostics.Process.GetProcessById(k);   //得到对进程k的引用  
+                fp.Kill();
+            }
+        }
+
+        public Boolean LoadFromExcel()
+        {
+            MSExcel.ApplicationClass excelApp = new MSExcel.ApplicationClass();
+            MSExcel._Workbook workBook = null;
+            MSExcel._Worksheet workSheet = null;
+            MSExcel.Range rng = null; //Ref https://msdn.microsoft.com/zh-cn/library/microsoft.office.interop.excel.range_members.aspx
+            object missing = System.Reflection.Missing.Value;
+            object readOnly = true;
+            try
+            {
+                // 打开Excel文件 
+                workBook = excelApp.Workbooks.Open(DOCPATH + @"MyExcel.xlsx", missing, readOnly, missing,
+                    missing, missing, missing, missing, missing,
+                    missing, missing, missing, missing, missing, missing);
+                //excelApp.Visible = true; // Excel应用程序可见 
+                // 针对Excel文档中的第一个Sheet就行操作。Sheet的下标从1开始 
+                workSheet = (MSExcel._Worksheet)workBook.Sheets[1];
+                workSheet.Activate();
+
+                //取得总记录行数 (包括标题列)
+                int N = 100;
+                int i, j;
+                for (i = 1; i <= N; i++)
+                {
+                    if (((MSExcel.Range)workSheet.Cells[1, i]).Value2 == null)
+                    {
+                        break;
+                    }
+                }
+                intCols = i - 1;
+                for (i = 1; i <= N; i++)
+                {
+                    if (((MSExcel.Range)workSheet.Cells[i, 1]).Value2 == null)
+                    {
+                        break;
+                    }
+                }
+                intRows = i - 1;
+                Log("Urika: " + intRows.ToString() + ", " + intCols.ToString());
+                Log("System: " + workSheet.UsedRange.Cells.Rows.Count + ", " + workSheet.UsedRange.Cells.Columns.Count);
+
+                //Read whole table, Ref http://blog.xudan123.com/121.html
+                strData = new String[intRows, intCols];
+                for (i = 1; i <= intRows; i++)
+                {
+                    for (j = 1; j <= intCols; j++)
+                    {
+                        rng = (Microsoft.Office.Interop.Excel.Range)workSheet.Cells[i, j];
+                        if (rng.Value2 == null)
+                        {
+                            Log("Row " + i + ", Col " + j + " Value is " + String.Empty);
+                            if (1 == j)
+                            {
+                                MessageBox.Show("Row " + i + ", Col " + j + ", " +strData[0, j - 1] + " must not be Null!");
+                                return false;
+                            }
+                            DialogResult dr = DialogResult.Yes;//MessageBox.Show(strData[i - 1, 0] + "'s " + strData[0, j - 1] + " (Row " + i + ", Col " + j + ") is Null! Continue?", "Warning", MessageBoxButtons.YesNo);
+                            if (dr == DialogResult.Yes){
+                                strData[i - 1, j - 1] = String.Empty;
+                                continue;
+                            }
+                            else if (dr == DialogResult.No){
+                                return false;
+                            }
+                        }
+                        //Use rng.Text other than rng.Value2 to Get the special TEXT
+                        strData[i - 1, j - 1] = rng.Text.ToString().Trim();
+                        Log("Row " + i + ", Col " + j + " Text is " + strData[i - 1, j - 1]);
+                    }
+                }
+
+                // 保存并关闭Excel工作簿 
+                workBook.Close(false, missing, missing);
+                // 退出Excel程序。如果不退出，可能出现，没有Excel文件打开，但是任务管理器中还有Excel进程 
+                excelApp.Quit();
+            }
+            catch (Exception e)
+            {
+                Log("Excel App error:" + e.ToString());
+                MessageBox.Show("Excel App error:" + e.ToString());
+            }
+            finally
+            {
+                if (excelApp != null)
+                {
+                    Kill(excelApp);
+                    Log("Kill excel app" + Environment.NewLine);
+                }
+            }
+            return true;
+        }
+
+        [DllImport("User32.dll", CharSet = CharSet.Auto)]
+        public static extern int GetWindowThreadProcessId(IntPtr hwnd, out int ID);
+        public static void Kill(MSExcel.Application excel)
+        {
+            IntPtr t = new IntPtr(excel.Hwnd);   //得到这个句柄，具体作用是得到这块内存入口 
+            int k = 0;
+            GetWindowThreadProcessId(t, out k);   //得到本进程唯一标志k
+            System.Diagnostics.Process p = System.Diagnostics.Process.GetProcessById(k);   //得到对进程k的引用
+            p.Kill();     //关闭进程k
+        }
+
+        public void Log(String s)
+        {
+            tbInfo.AppendText(s + Environment.NewLine);
+            tbInfo.ScrollToCaret();
+        }
+
+    }
+}
